61e674590c62c7362491e38032331be0af308629
Silvio Silva | Stone <silvio.silva@stone.local@BRRIOLN012383.stone.local>
2019-02-09 21:09:20 -0200

    Added defaultdict

--- scaleio_exporter/scl_parse.py
+++ scaleio_exporter/scl_parse.py
@@ -2,8 +2,9 @@
 # -*- coding: utf-8 -*-
 
 
+import socket
 from collections import defaultdict
-from subprocess import check_output
+from subprocess import check_output, STDOUT
 from scaleio_exporter.scl_conn import connect_scaleio
 
 
@@ -31,7 +32,18 @@ class scaleio_data(object):
         self.query = "{} {} {} --mdm_ip {} --mdm_port {}".format(
             self.conn.scli, self.params, self.properties, self.mdm_ips, self.mdm_port
         )
+        self.data_dict = defaultdict(lambda: False)
+        self.hostname = socket.gethostname()
+        self.data_dict[self.hostname] = defaultdict(lambda: False)
 
     def read_data(self):
-        raw_data = check_output([self.query], shell=True)
-        print(raw_data)
+        raw_data = check_output([self.query], shell=True, stderr=STDOUT)
+        storages = list(filter(None, raw_data.split("STORAGE_POOL".encode())))
+        for storage in storages:
+            s_storage = list(filter(None, storage.split("\n".encode())))
+            d_storage = defaultdict(lambda: False)
+            for line in s_storage[1:]:
+                s_line = line.strip().split()
+                d_storage[s_line[0].decode()] = s_line[1].decode()
+            self.data_dict[self.hostname][s_storage[0].decode().strip().rstrip(":")] = d_storage
+        return self.data_dict
